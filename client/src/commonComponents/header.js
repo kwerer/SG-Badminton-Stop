6b5dea2495f63346bb238dc009a000f8856085e2
Joseph <josephlailailai247@gmail.com>
2021-12-28 22:36:21 +0800

    added cards

--- client/src/commonComponents/header.js
+++ client/src/commonComponents/header.js
@@ -1,26 +1,87 @@
 import React from "react";
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
+import {
+  Navbar,
+  Nav,
+  Image,
+  Container,
+  Form,
+  FormControl,
+  Button,
+} from "react-bootstrap";
 import styles from "./styles.module.css";
+import Logo from "../Images/Rectangle-logo-without-bg.png";
+import { Link } from "react-router-dom";
 
 export default function Header() {
+  const NavLinks = [
+    {
+      title: "Look for games",
+      link: "games",
+      disabled: false,
+    },
+    {
+      title: "About",
+      link: "about",
+      disabled: false,
+    },
+    {
+      title: "News (Coming Soon!)",
+      link: "News",
+      disabled: true,
+    },
+    {
+      title: "Shop (Coming Soon!)",
+      link: "Shop",
+      disabled: true,
+    },
+  ];
   return (
-    <Navbar bg="light" variant="light">
-      <Container
-        fluid
-        className={styles.HeaderContainer}
+    <>
+      <Navbar
+        bg="light"
+        variant="light"
+        expand="lg"
       >
-        <Nav className={styles.HeaderContainer}>
-          <Nav.Link href="#games">Games</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-          <Navbar.Brand href="#home">
-            Image
+        <Container fluid>
+          <Navbar.Brand as={Link} to="">
+            <Image
+              src={Logo}
+              className={styles.HeaderImage}
+            />
           </Navbar.Brand>
-          <Nav.Link href="#news">News</Nav.Link>
-          <Nav.Link href="#shop">Shop</Nav.Link>
-        </Nav>
-      </Container>
-    </Navbar>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse id="navbarScroll">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              {NavLinks.map((val, key) => {
+                return (
+                  <Nav.Link
+                    as={Link}
+                    to={val.link}
+                    disabled={val.disabled}
+                  >
+                    {val.title}
+                  </Nav.Link>
+                );
+              })}
+            </Nav>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">
+                Search
+              </Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
--- client/src/routes/home/home.js
+++ client/src/routes/home/home.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import GameCard from "../../commonComponents/GameCard";
+import styles from "./styles.module.css";
 
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <div className={styles.MainDiv}>
+        <GameCard />
+      </div>
+    </>
+  );
 }
